1771ce7383c62e8201cdf87862e5f6843c31acb5
adi.finkelman <adi.finkelman@hibob.io>
2024-09-11 10:09:33 +0300

    JWT - review changes

--- src/main/kotlin/com/hibob/academy/filters/AuthenticationFilter.kt
+++ src/main/kotlin/com/hibob/academy/filters/AuthenticationFilter.kt
@@ -8,19 +8,24 @@ import jakarta.ws.rs.container.ContainerRequestContext
 import jakarta.ws.rs.container.ContainerRequestFilter
 import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.ext.Provider
+import org.apache.http.auth.AUTH
 import org.springframework.stereotype.Component
 import javax.crypto.SecretKey
+
+const val LOGIN_PATH = "adi/usersession/login"
+const val AUTH = "Authorization"
 
 @Component
 @Provider
 class AuthenticationFilter(private val sessionService: SessionService) : ContainerRequestFilter {
+
     override fun filter(requestContext: ContainerRequestContext) {
 
-        if (requestContext.uriInfo.path == "adi/usersession/login") {
+        if (requestContext.uriInfo.path == LOGIN_PATH) {
             return
         }
-        val authHeader = requestContext.cookies["Authorization"]?.value?.trim()
-        val token = authHeader?.substringAfter("Bearer ")
+        val authCookie = requestContext.cookies[AUTH]?.value?.trim()
+        val token = authCookie?.substringAfter("Bearer ")
 
         if (verify(token) == null) {
             requestContext.abortWith(
