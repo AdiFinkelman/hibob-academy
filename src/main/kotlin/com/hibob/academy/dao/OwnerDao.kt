55b7a14c293e31e7fa78ca5cf2b682ee0df92c81
adi.finkelman <adi.finkelman@hibob.io>
2024-09-11 08:35:09 +0300

    Jooq read - add tests

--- src/main/kotlin/com/hibob/academy/dao/OwnerDao.kt
+++ src/main/kotlin/com/hibob/academy/dao/OwnerDao.kt
@@ -1,2 +1,24 @@
 package com.hibob.academy.dao
 
+import jakarta.inject.Inject
+import org.jooq.DSLContext
+import org.jooq.Record
+import org.jooq.RecordMapper
+
+class OwnerDao @Inject constructor(private val sql: DSLContext) {
+
+    private val owner = OwnerTable.instance
+
+    private val ownerMapper = RecordMapper<Record, OwnerData>
+    { record ->
+        OwnerData (
+            record[owner.name],
+            record[owner.employeeId]
+        )
+    }
+
+    fun getOwner(): List<OwnerData> =
+        sql.select(owner.name, owner.employeeId, owner.companyId)
+            .from(owner)
+            .fetch(ownerMapper)
+}
