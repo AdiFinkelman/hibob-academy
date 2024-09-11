a5da52b9a7a59fcafbaf27a53a1f65a7af637795
adi.finkelman <adi.finkelman@hibob.io>
2024-09-11 21:13:53 +0300

    SQL_2 - update indexes

---
+++ src/main/resources/db/migration/V202409112011__update_indexes.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_owner_company_employee_id;
+DROP INDEX IF EXISTS idx_pets_company_id;
+
+CREATE INDEX idx_pets_company_id ON pet(company_id);
+CREATE UNIQUE INDEX idx_owner_company_employee_id ON owner(company_id, employee_id);
