4411bfe51e2270cab99b00ce7bc0d22867beb589
adi.finkelman <adi.finkelman@hibob.io>
2024-09-25 12:23:35 +0300

    delete unused parameter

--- src/main/kotlin/com/hibob/project/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/project/resource/feedbackResource.kt
@@ -1,24 +1,19 @@
 package com.hibob.project.resource
 
 import com.hibob.project.dao.*
-import com.hibob.project.service.AuthenticationService
 import com.hibob.project.service.FeedbackService
 import jakarta.ws.rs.*
 import jakarta.ws.rs.container.ContainerRequestContext
 import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 
 @Controller
 @Path("/api/system/feedback")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class FeedbackResource(
-    private val feedbackService: FeedbackService,
-    private val authService: AuthenticationService
-) {
+class FeedbackResource(private val feedbackService: FeedbackService) {
     @GET
     @Path("/{companyId}")
     fun getAllFeedbacks(@PathParam("companyId") companyId: Long) = feedbackService.getAllFeedbacks(companyId)
