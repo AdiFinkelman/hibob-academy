e3f2990701ef61baf4851022fcf796b645d41966
adi.finkelman <adi.finkelman@hibob.io>
2024-09-26 00:35:38 +0300

    Create service and resource

--- src/main/kotlin/com/hibob/project/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/project/resource/feedbackResource.kt
@@ -40,4 +40,22 @@ class FeedbackResource(private val feedbackService: FeedbackService) {
 
         return Response.ok(feedbackCreationRequest).build()
     }
+
+    @PUT
+    @Path("/status/{feedbackId}")
+    fun markFeedbackStatue(@Context requestContext: ContainerRequestContext, @PathParam("feedbackId") feedbackId: Long, statusResponse: StatusResponse): Response {
+        val authenticatedEmployee = AuthenticationUtil.extractAuthenticatedEmployee(requestContext)
+        feedbackService.markFeedbackStatus(feedbackId, statusResponse.status, authenticatedEmployee.companyId)
+
+        return Response.ok().build()
+    }
+
+    @GET
+    @Path("/status/{feedbackId}")
+    fun checkFeedbackStatus(@Context requestContext: ContainerRequestContext, @PathParam("feedbackId") feedbackId: Long,): Response {
+        val authenticatedEmployee = AuthenticationUtil.extractAuthenticatedEmployee(requestContext)
+        val statusResponse = feedbackService.checkFeedbackStatus(feedbackId, authenticatedEmployee.companyId)
+
+        return Response.ok(statusResponse.status).build()
+    }
 }
--- src/main/kotlin/com/hibob/project/service/feedbackService.kt
+++ src/main/kotlin/com/hibob/project/service/feedbackService.kt
@@ -1,6 +1,8 @@
 package com.hibob.project.service
 
 import com.hibob.project.dao.*
+import jakarta.ws.rs.BadRequestException
+import jakarta.ws.rs.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -15,7 +17,29 @@ class FeedbackService @Autowired constructor(private val feedbackDao: FeedbackDa
         if (validateFeedback(feedbackCreationRequest)) { feedbackDao.feedbackSubmission(feedbackCreationRequest, employee) }
     }
 
-    fun validateFeedback(feedbackCreationRequest: FeedbackCreationRequest): Boolean {
+    fun markFeedbackStatus(feedbackId: Long, status: StatusType, companyId: Long) {
+        val feedback = feedbackDao.getAllFeedbacks(companyId).find { it.id == feedbackId }
+            ?: throw NotFoundException("Feedback not found")
+
+        if (!feedback.isAnonymous) {
+            feedbackDao.markFeedbackStatus(feedback, status)
+        }
+        else throw BadRequestException("Feedback marked as anonymous")
+    }
+
+    fun checkFeedbackStatus(feedbackId: Long, companyId: Long): StatusResponse {
+        val feedback = feedbackDao.getAllFeedbacks(companyId).find { it.id == feedbackId }
+            ?: throw NotFoundException("Feedback not found")
+
+        if (!feedback.isAnonymous) {
+            val statusResponse = feedbackDao.checkStatus(feedback)
+
+            return statusResponse
+        }
+        throw BadRequestException("Feedback marked as anonymous")
+    }
+
+    private fun validateFeedback(feedbackCreationRequest: FeedbackCreationRequest): Boolean {
         if (feedbackCreationRequest.text.length < 2)
             throw IllegalArgumentException("Text must be at least 2 characters")
         else if (feedbackCreationRequest.text.length > 100)
