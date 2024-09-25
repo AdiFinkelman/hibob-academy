867b38124392a12fb6141e0d341e858c331c186e
adi.finkelman <adi.finkelman@hibob.io>
2024-09-25 10:25:54 +0300

    create tests

--- src/test/kotlin/com/hibob/project/dao/CommentDaoTest.kt
+++ src/test/kotlin/com/hibob/project/dao/CommentDaoTest.kt
@@ -14,30 +14,32 @@ class CommentDaoTest @Autowired constructor(private val sql: DSLContext) {
     private val commentDao = CommentDao(sql)
     private val feedbackDao = FeedbackDao(sql)
     private val commentTable = CommentTable.instance
+    private val feedbackTable = FeedbackConfigurationTable.instance
     private val userTest = Employee(1, "Adi", "Finkelman", Role.EMPLOYEE, 1234L, "development")
+    private val feedbackCreation = FeedbackCreationRequest("Complaint", Timestamp.valueOf(LocalDateTime.now().withNano(0)), false, StatusType.UNREVIEWED)
+    private val feedbackIdTest = feedbackDao.feedbackSubmission(feedbackCreation, userTest)
+
 
     @Test
-    fun `get all comments from feedback successfully`() {
-        val feedbackCreation = FeedbackCreationRequest("Complaint", Timestamp.valueOf(LocalDateTime.now().withNano(0)), false, StatusType.UNREVIEWED)
-        val feedbackId = feedbackDao.feedbackSubmission(feedbackCreation, userTest)
-        val commentCreation1 = CommentCreationRequest(userTest.id, "Let me help you", feedbackId, Timestamp.valueOf(LocalDateTime.now().withNano(0)))
-        val commentCreation2 = CommentCreationRequest(userTest.id, "Hope you feel better", feedbackId, Timestamp.valueOf(LocalDateTime.now().withNano(0)))
-        val commentId1 = commentDao.respondToFeedback(commentCreation1, feedbackId)
-        val commentId2 = commentDao.respondToFeedback(commentCreation2, feedbackId)
+    fun `add comments to feedback and get all comments from feedback successfully`() {
+        val commentCreation1 = CommentCreationRequest(userTest.id, "Let me help you", feedbackIdTest, Timestamp.valueOf(LocalDateTime.now().withNano(0)))
+        val commentCreation2 = CommentCreationRequest(userTest.id, "Hope you feel better", feedbackIdTest, Timestamp.valueOf(LocalDateTime.now().withNano(0)))
+        val commentId1 = commentDao.respondToFeedback(commentCreation1, feedbackIdTest)
+        val commentId2 = commentDao.respondToFeedback(commentCreation2, feedbackIdTest)
         val comment1 = extractToComment(commentCreation1, commentId1)
         val comment2 = extractToComment(commentCreation2, commentId2)
-        assertEquals(listOf(comment1, comment2), commentDao.getAllComments(feedbackId))
+        assertEquals(listOf(comment1, comment2), commentDao.getAllComments(feedbackIdTest))
     }
 
     @Test
-    fun `get all comments from feedback where comment list is empty`() {
-        val feedbackCreation = FeedbackCreationRequest("Complaint", Timestamp.valueOf(LocalDateTime.now().withNano(0)), false, StatusType.UNREVIEWED)
-        val feedbackId = feedbackDao.feedbackSubmission(feedbackCreation, userTest)
-        assertEquals(emptyList<Comment>(), commentDao.getAllComments(feedbackId))
+    fun `get empty comment list from feedback where comment list is empty`() {
+        assertEquals(emptyList<Comment>(), commentDao.getAllComments(feedbackIdTest))
     }
 
     @Test
-    fun `create comment when feedback is not exists and throws exception`() {}
+    fun `get empty comment list from feedback when feedback is not exist`() {
+        assertEquals(emptyList<Comment>(), commentDao.getAllComments(1111))
+    }
 
     private fun extractToComment(commentCreation: CommentCreationRequest, id: Long): Comment {
         val comment = Comment(
@@ -53,8 +55,19 @@ class CommentDaoTest @Autowired constructor(private val sql: DSLContext) {
 
     @AfterEach
     fun cleanup() {
-        sql.delete(commentTable)
-            .where(commentTable.feedbackId.eq(feedbackIdTest))
+        deleteCommentTable(commentTable, feedbackIdTest)
+        deleteFeedbackTable(feedbackTable, userTest.companyId)
+    }
+
+    private fun deleteCommentTable(table: CommentTable, feedbackId: Long) {
+        sql.delete(table)
+            .where(table.feedbackId.eq(feedbackId))
+            .execute()
+    }
+
+    private fun deleteFeedbackTable(table: FeedbackConfigurationTable, companyId: Long) {
+        sql.delete(table)
+            .where(table.companyId.eq(companyId))
             .execute()
     }
 }
